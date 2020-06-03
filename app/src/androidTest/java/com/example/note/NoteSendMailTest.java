ab8fddb6a49cd0a2000ab76f6224212158f6e668
Florian Haid <florian.haid@student.tugraz.at>
2020-06-03 09:30:06 +0200

    NOTE-009B added Test to check if Share-Functionality works [DA, FH]

---
+++ app/src/androidTest/java/com/example/note/NoteSendMailTest.java
@@ -0,0 +1,119 @@
+package com.example.note;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.note.controller.NoteManager;
+import com.example.note.dao.NoteDAO;
+import com.example.note.db.AppDatabase;
+import com.example.note.db.DatabaseHelper;
+import com.example.note.model.Note;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.RootMatchers.isDialog;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class NoteSendMailTest {
+
+    private AppDatabase testDB;
+    private NoteDAO noteDAO;
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivity = new IntentsTestRule<>(MainActivity.class);
+
+    @Before
+    public void initDB() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        DatabaseHelper.getInstance().initDb(appContext);
+        testDB = DatabaseHelper.getInstance().getDb();
+        noteDAO = testDB.noteDAO();
+        noteDAO.insertNotes();
+    }
+
+    @After
+    public void closeDB() {
+        List<Note> notes = noteDAO.getNotes();
+        for(Note note : notes) {
+            noteDAO.deleteNote(note);
+        }
+        testDB.close();
+    }
+
+    @Test
+    public void sendEmailTest() {
+        String title = "Test1";
+        String content = "Hallo, ich bin ein Testnote!";
+        String tag = "sport";
+
+        NoteManager noteManager = NoteManager.getInstance();
+        int result = noteManager.addNote(title, content, tag);
+        assertEquals(result, 0);
+
+        String test_subject = "Note: " + tag;
+        String test_content = "Here is the note I was talking about:\n" +
+                "\nTitle: " + tag +
+                "\n\nContent: " + content +
+                "\n\nTag: " + tag;
+
+        onView(withId(R.id.show_done_notes))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withId(R.id.overview_back_all_notes_btn))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withId(R.id.overview_list))
+                .check(matches(isDisplayed()));
+
+        onData(anything())
+                .inAdapterView(withId(R.id.overview_list))
+                .atPosition(0)
+                .onChildView(withId(R.id.overview_item_more_btn))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withText("Send via e-mail"))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        intended(allOf(hasAction(Intent.ACTION_CHOOSER),
+                hasExtra(Intent.EXTRA_TITLE, "Choose client: "),
+                hasExtra(is(Intent.EXTRA_INTENT),
+                        hasAction(Intent.ACTION_SEND))));
+
+        /*intended(allOf(hasAction(Intent.ACTION_CHOOSER),
+                hasExtra(is(Intent.EXTRA_INTENT),
+                        allOf( hasAction(Intent.ACTION_SEND),
+                               hasExtra("typ", "message/rfc882")
+                               hasExtra(Intent.EXTRA_SUBJECT, test_subject),
+                               hasExtra(Intent.EXTRA_TEXT, test_content) ))));*/
+    }
+
+}
